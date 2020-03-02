fdebb49b98dd68b68d6e54871e4c412ba43a4aa7
Mo Russ <mauriceruss302@gmail.com>
2020-03-02 16:22:32 -0500

    start person class

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,5 +1,19 @@
 package io.zipcoder.interfaces;
 
 public class Person {
+    private  Long id;
+    private  String name;
 
+    public Person(Long id, String name){
+        this. id = id;
+        this.name = name;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
 }
