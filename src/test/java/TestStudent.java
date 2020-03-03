095fde74d269a89b4cb28af4ec17557d4a1f5500
Mo Russ <mauriceruss302@gmail.com>
2020-03-03 08:28:43 -0500

    TestStudent stubs made

--- src/test/java/TestStudent.java
+++ src/test/java/TestStudent.java
@@ -1,2 +1,19 @@
+import org.junit.Test;
+
 public class TestStudent {
+
+    @Test
+    public void testImplementation(){
+
+    }
+
+    @Test
+    public void testInheritance(){
+
+    }
+
+    @Test
+    public void testLearn(){
+        
+    }
 }
