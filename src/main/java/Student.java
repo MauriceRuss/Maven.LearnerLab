25afae05e88244f67bc8c8fa0c41db0b6cd8d180
Mo Russ <mauriceruss302@gmail.com>
2020-03-02 22:23:27 -0500

    Student class started

---
+++ src/main/java/Student.java
@@ -0,0 +1,11 @@
+import io.zipcoder.interfaces.Person;
+
+public class Student implements Learner {
+    public void learn(double numberOfHours) {
+
+    }
+
+    public Double getTotalStudyTime() {
+        return null;
+    }
+}
