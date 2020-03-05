f1f812da1447387540834f4a5c9b46d89edc1d7c
Mo Russ <mauriceruss302@gmail.com>
2020-03-04 20:56:40 -0500

    Student tests written

--- src/test/java/TestStudent.java
+++ src/test/java/TestStudent.java
@@ -1,19 +1,32 @@
+import io.zipcoder.interfaces.Person;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestStudent {
 
     @Test
     public void testImplementation(){
-
+    Student pat = new Student();
+    Double expected = 5.0;
+    Double actual =pat.getTotalStudyTime();
+    Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testInheritance(){
+    Person pat = new Student();
+    String expected = "pat";
+    String actual = pat.getName();
+    Assert.assertEquals(expected, actual);
 
     }
 
     @Test
     public void testLearn(){
-        
+    Student pat = new Student();
+    Double expected = 5.0;
+    pat.learn(5);
+    Double actual = pat.getTotalStudyTime();
+    Assert.assertEquals(expected,actual);
     }
 }
