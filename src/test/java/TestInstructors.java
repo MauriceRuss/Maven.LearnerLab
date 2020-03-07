95af2409689b8e17a711e288fffc5d44c46b9f20
Mo Russ <mauriceruss302@gmail.com>
2020-03-07 16:39:10 -0500

    testInstructors passed

--- src/test/java/TestInstructors.java
+++ src/test/java/TestInstructors.java
@@ -1,2 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestInstructors {
+
+    @Test
+    public void testInstructors(){
+        Instructors cohort =  Instructors.getInstance();
+        cohort.add(new Instructor(1321, "joey"));
+        cohort.add(new Instructor(11, "mark"));
+        cohort.add(new Instructor(21, "steve"));
+        int expectedSize = 3;
+        int actualSize = cohort.countListSize();
+        Assert.assertEquals(expectedSize, actualSize);
+
+    }
 }
